66b4b053a054a845e5cc62194c7d5467619ee6d7
Theo Pack <tf.pack@gmail.com>
2014-06-27 11:41:22 +0200

    change mongo prompt

--- hacks/prompt.js
+++ hacks/prompt.js
@@ -1,20 +1,10 @@
 // Improve the default prompt with hostname, process type, and version
-prompt = function() {
-    var serverstatus = db.serverStatus();
-    var host = serverstatus.host.split('.')[0];
-    var process = serverstatus.process;
-    var version = db.serverBuildInfo().version;
-    var repl_set = db._adminCommand({"replSetGetStatus": 1}).ok !== 0;
-    var rs_state = '';
-    if(repl_set) {
-        members = rs.status().members;
-        for(var i = 0; i<members.length; i++){
-            if(members[i].self === true){
-                rs_state = '[' + members[i].stateStr + ']';
-            }
-        };
-    }
-    var state = isMongos() ? '[mongos]' : rs_state;
-    return host + '(' + process + '-' + version + ')' + state + ' ' + db + '> ';
-};
-
+var host = db.serverStatus().host.substr(0,4);
+if (host === "coin") {
+  host = "🐳";
+} else if (host === "naga") {
+  host = "🐠";
+} else {
+  host = "🐬";
+}
+prompt = function() { return db+" at " + host + "  > "; }
